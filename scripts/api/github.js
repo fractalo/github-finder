5e6e731be2959bb92271f03d9cf8406392c8bfc0
fractalo <42487844+fractalo@users.noreply.github.com>
2024-02-21 11:40:05 +0900

    feat: github users api

--- scripts/api/github.js
+++ scripts/api/github.js
@@ -1,6 +1,43 @@
 
+const BASE_URL = 'https://api.github.com';
+
+
 export class Github {
-    constructor() {
-        
+    constructor() { 
     }
+
+    async _callApi(path, method, config) {
+        const { params } = config;
+
+        const url = new URL(path, BASE_URL);
+        url.search = new URLSearchParams(params).toString();
+
+        const response = await fetch(
+            url,
+            {
+                headers: {
+                    accept: 'application/vnd.github+json',
+                },
+                method,
+                body: config.body,
+            }
+        );
+
+        return response.json();
+    }
+
+    async searchUsers(query, pageNum = 1, pageSize = 30) {
+        return this._callApi('/search/users', 'GET', {
+            params: {
+                query,
+                page: pageNum,
+                per_page: pageSize,
+            }
+        });
+    }
+
+    async getUser(username) {
+        return this._callApi(`/users/${username}`, 'GET');
+    }
+
 }
